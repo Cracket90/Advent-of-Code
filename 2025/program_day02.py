432d3d75946aced74a66ad88fb1a752a4d115836
AndreL <123906265+Cracket90@users.noreply.github.com>
2025-12-02 14:58:10 +0100

    Add files via upload

---
+++ 2025/program_day02.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  2 11:48:58 2025
+
+@author: andre
+"""
+
+# from math import floor, log10
+
+def find_invalid_IDs1(input_file: str) -> int:
+    somma = 0
+    with open(input_file, encoding= 'utf8') as f:
+        intervalli = f.read().strip().split(',')
+    for intervallo in intervalli:
+        start, end = [int(valore) for valore in intervallo.split('-')]
+        invalid_IDs = [valore for valore in range(start, end+1) if is_twice(valore)]
+        somma += sum(invalid_IDs)
+    return somma
+
+def is_twice(valore: int) -> bool:
+    # floor(log10(valore)+1) % 2 == 0
+    valore_str = str(valore)
+    len_valore = len(valore_str)
+    if len_valore % 2 == 0 and valore_str[:len_valore//2] == valore_str[len_valore//2:]:
+        return True
+    return False
+
+def find_invalid_IDs2(input_file: str) -> int:
+    somma = 0
+    with open(input_file, encoding= 'utf8') as f:
+        intervalli = f.read().strip().split(',')
+    for intervallo in intervalli:
+        start, end = [int(valore) for valore in intervallo.split('-')]
+        invalid_IDs = [valore for valore in range(start, end+1) if is_repeated(valore)]
+        somma += sum(invalid_IDs)
+    return somma
+
+def is_repeated(valore: int) -> bool:
+    valore_str = str(valore)
+    len_valore = len(valore_str)
+    for len_pattern in range(len_valore//2, 0, -1):
+        if len_valore % len_pattern == 0:
+            pattern = valore_str[:len_pattern]
+            for index in range(len_pattern, len_valore, len_pattern):
+                window = valore_str[index: index+len_pattern]
+                if window != pattern:
+                    break
+            else:
+                return True
+    return False
+
+
+if __name__ == '__main__':
+    print(find_invalid_IDs1('input_day2.txt'))
+    print(find_invalid_IDs2('input_day2.txt'))
