69fc0692cb1b2fccabd27ee88017437ee225508f
AndreL <123906265+Cracket90@users.noreply.github.com>
2025-12-04 23:53:52 +0100

    Add files via upload

--- 2025/program_day04.py
+++ 2025/program_day04.py
@@ -58,29 +58,61 @@ def remove_rolls(grid: list[list[str]], height: int, width: int) -> int:
 
 def find_rolls2_faster(input_file: str) -> int:
     dirs = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
-    total = 0
     paper_roll = '@'
-    removed = 'x'
     with open(input_file, encoding= 'utf8') as f:
         grid = [list(line) for line in f.read().splitlines()]
     H = len(grid)
     W = len(grid[0])
-    while True:
-        changed = False
-        for y in range(H):
-            for x in range(W):
-                if grid[y][x] == paper_roll:
-                    close_rolls = 0
-                    for dy, dx in dirs:
-                        if 0 <= y+dy < H and 0 <= x+dx < W:
-                            close_rolls += (grid[y+dy][x+dx] == paper_roll)
-                    if close_rolls < 4:
-                        grid[y][x] = removed
-                        total += 1
-                        changed = True
-        if not(changed):
-            break
-    return total
+    rolls: dict[tuple[int,int],int] = {}
+    accessible_rolls: list[tuple[int,int]] = []
+    for y in range(H):
+        for x in range(W):
+            if grid[y][x] == paper_roll:
+                close_rolls = 0
+                for dy, dx in dirs:
+                    if 0 <= y+dy < H and 0 <= x+dx < W:
+                        close_rolls += (grid[y+dy][x+dx] == paper_roll)
+                if close_rolls < 4:
+                    accessible_rolls.append((y, x))
+                rolls[(y, x)] = close_rolls
+    starting_rolls = len(rolls)
+    while accessible_rolls:
+        removed = accessible_rolls.pop()
+        rolls.pop(removed)
+        y, x = removed
+        for dy, dx in dirs:
+            ny, nx = y+dy, x+dx
+            if (ny, nx) in rolls:
+                rolls[(ny, nx)] -= 1
+                if rolls[(ny, nx)] == 3:
+                    accessible_rolls.append((ny, nx))
+    return starting_rolls - len(rolls)
+
+# def find_rolls2_faster(input_file: str) -> int:
+#     dirs = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
+#     total = 0
+#     paper_roll = '@'
+#     removed = 'x'
+#     with open(input_file, encoding= 'utf8') as f:
+#         grid = [list(line) for line in f.read().splitlines()]
+#     H = len(grid)
+#     W = len(grid[0])
+#     while True:
+#         changed = False
+#         for y in range(H):
+#             for x in range(W):
+#                 if grid[y][x] == paper_roll:
+#                     close_rolls = 0
+#                     for dy, dx in dirs:
+#                         if 0 <= y+dy < H and 0 <= x+dx < W:
+#                             close_rolls += (grid[y+dy][x+dx] == paper_roll)
+#                     if close_rolls < 4:
+#                         grid[y][x] = removed
+#                         total += 1
+#                         changed = True
+#         if not changed:
+#             break
+#     return total
 
 
 if __name__ == '__main__':
